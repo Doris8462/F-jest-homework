06bf7706da6e8875cac1c2d78394555d4e4b1b77
Doris8462 <liandou123>
2020-09-15 17:05:13 +0800

    [haungyunjie] feat:finish

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,29 @@
+import axios from "axios";
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    axios.post.mockResolvedValueOnce({
+      data: {
+        name: "NANA",
+      },
+    });
+    const result = register("mocked username", "mocked password");
+
+    await expect(result).resolves.toEqual({ name: "NANA" });
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockImplementation(() => false);
+    const username = "mocked username";
+    const password = "mocked password";
+    const result = register(username, password);
+    await expect(result).rejects.toThrow("wrong username or password");
   });
 });
